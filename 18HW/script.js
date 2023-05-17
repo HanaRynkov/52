d518b272ad4671025ff8b9f306958827e0da7e0e
Hana Rynkov <hanarynkov@Eduards-MacBook-Pro.local>
2023-05-17 12:24:55 +0300

    HW_19

--- 18HW/script.js
+++ 18HW/script.js
@@ -4,7 +4,7 @@
 // денег со счета withdraw(), а также способ проверки баланса checkBalance().
 
 const bankAccount = {
-    accountNumber: 178,
+    accountNumber: '178',
     accountHolderName: 'Hanna_Rynkov',
     balance: 1000,
     checkBalance: function () {
@@ -17,6 +17,30 @@ const bankAccount = {
         this.balance = thise.balance - addNum;
     },
 };
+
+
+const bankAccount = {
+    accountNumber: '178',
+    accountHolderName: 'Hanna_Rynkov',
+    balance: 1000,
+    deposit(sum) {
+        if (sum < = 0) return;
+        sum > 10000 ?
+            console.log('Требуется подтверждение легальности доходов') :
+            this.balance += sum;
+
+    },
+    withdraw(sum) {
+        sum <= thise.balance ?
+            thise.balance -= sum :
+            console.log('Insufficient funds');
+    }
+
+    checkBalance() {
+        console.log(this.balance);
+    };
+};
+
 
 bankAccount.checkBalance(); // Account balance is 1000
 bankAccount.deposit(500);
@@ -35,18 +59,38 @@ bankAccount.withdraw(1000); // Insufficient funds
 // должно быть суммой цены каждого предмета, умноженной на его количество.
 
 const shoppingCart = {
-    items: [],
-    totalCost: 0,
+    items: [], // массив товаров 
+    totalCost: 0,// суммарная стоимость товаров 
     addItem(item) {
+        const existingItem = this.items.find(e => e.name === item.name); // итерируем (перебираем) массив айтемов e это каждый элемент
 
+        if (existingItem) {
+            existingItem.quantity += item.quantity;
+        } else {
+            if (itemName && item.price > 0 && item.quantity > 0) {
+                this.items.push(item);
+            }
+        }
     },
     removeItem(itemName) {
-
+        const index = this.items.findIndex(e => e.name === itemName);
+        if (index !== -1) {
+            this.items.splice(index, 1); // индекс какой элемент мы хотим удалить а 1 это сколько штук мы хотим удалить
+            //this.items = this.items.filter((e, i) => i !== index);
+        }
+        this.updateTotalCost();
     },
+
+
     updateTotalCost() {
+        this.totalCost = this.items.reduce((acc, item) =>  acc + item.quantity * item.price);
+    }
 
-    },
-};
+
+}
+
+
+
 
 shoppingCart.addItem({ name: "Shirt", price: 20, quantity: 2 });
 shoppingCart.addItem({ name: "Pants", price: 30, quantity: 1 });
---
+++ 19/script.js
@@ -0,0 +1,107 @@
+const shoppingCart = {
+    items: [], // массив товаров 
+    totalCost: 0,// суммарная стоимость товаров 
+    addItem(item) {
+        const existingItem = this.items.find(e => e.name === item.name); // итерируем (перебираем) массив айтемов e это каждый элемент
+
+        if (existingItem) {
+            existingItem.quantity += item.quantity;
+        } else {
+            if (item.name && item.price > 0 && item.quantity > 0) {
+                this.items.push(item);
+            }
+        }
+    },
+    removeItem(itemName) {
+        const index = this.items.findIndex(e => e.name === itemName);
+        if (index !== -1) {
+            this.items.splice(index, 1); // индекс какой элемент мы хотим удалить а 1 это сколько штук мы хотим удалить
+            //this.items = this.items.filter((e, i) => i !== index);
+        }
+        this.updateTotalCost();
+    },
+
+
+    updateTotalCost() {
+        this.totalCost = this.items.reduce((acc, item) => acc + item.quantity * item.price);
+    }
+
+
+}
+
+
+
+const addButton = document.querySelector('#add');
+const statsButton = document.getElementById('stats');
+const productList = document.getElementById('productList');
+console.log(addButton, statsButton);
+addButton.onclick = addHandler;
+
+statsButton.onclick = stastHandler;
+
+//addButton.addEventListener('click', addHandler);
+addButton.addEventListener('keydown', (e) => {
+    //console,log(e);
+    if (e.key === 'Enter') {
+        addHandler();
+    }
+});
+
+function addHandler() {
+    const productName = document.getElementById('productName');
+    const price = document.getElementById('price');
+    const quantity = document.getElementById('quantity');
+    shoppingCart.addItem({
+        name: productName.value.trim(),
+        price: +price.value,
+        quantity: +quantity.value
+    })
+
+    productList.innerHTML = '';
+    shoppingCart.items.forEach(e => {
+        const li = document.createElement('li');
+        li.textContent = `Product name: ${e.name}, Product price: ${e.price}, quantity of product: ${e.quantity}`;
+        productList.append(li);
+    })
+}
+
+function stastHandler() {
+    let totalQty = 0;
+    let totalCost = 0;
+
+
+
+    shoppingCart.items.forEach(e => {
+        totalQty += e.quantity;
+        totalCost += e.price * e.quantity;
+
+    })
+
+    const prices = shoppingCart.items.map(e => {
+        return e.price;
+    })
+
+    const max = Math.max(...prices);
+    const min = Math.min(...prices);
+    const avg = totalCost/totalQty;
+
+    console.log(totalQty, totalCost,max,min);
+
+
+    alert(`
+        Total Qty: ${totalQty} \n
+        Total Cost: ${totalCost} \n
+        Min Cost: ${min} \n
+        Max Cost: ${max} \n
+        Avg Cost: ${avg.toFixed(2)} \n
+        
+    `)
+
+    //ассортимент (количество позиций товаров)
+    // totalCoast of all products
+    //quantity of all products
+    //max price of prod
+    //min price of prod
+    //avg price of prod 
+
+}
