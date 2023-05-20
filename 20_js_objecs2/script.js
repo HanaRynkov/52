001874cf034316d699d0a0f74dd4a7c33162d75c
Hana Rynkov <hanarynkov@Eduards-MacBook-Pro.local>
2023-05-21 00:25:57 +0300

    HW_20

--- 20_js_objecs2/script.js
+++ 20_js_objecs2/script.js
@@ -5,7 +5,7 @@ const phoneBook = {
         email: '',
         phoneNumber: '',
         company: '',
-        addres: {
+        address: {
             street: '',
             house: '',
             city: ''
@@ -15,30 +15,57 @@ const phoneBook = {
         if (contact.userName) {
             contact.id = new Date().getTime();
             this.contactsList.push(contact);
-            alert('Contact added');
+            alert('Contact added!');
+        }
+    },
+    changeContact(updatedContact) {
+        const index = this.contactsList.findIndex(contact => {
+            contact.userName === updatedContact.userName
+        })
+        if (index !== -1) {
+            this.contactsList[index] = updatedContact;
+        } else {
+            alert('Contact not found...');
+        }
+    },
+    deleteContact(userName) {
+        const index = this.contactsList.findIndex(contact => {
+            contact.userName === userName;
+        });
+        if (index !== -1) {
+            this.contactsList.splice(index, 1);
+        } else {
+            alert('Contact not found...');
         }
     }
 }
 
-changeContact(userName, updatedContact) {
-    const index = this.contactsList.findIndex(contact => {
-        contact.userName === updatedContact.userName
+
+const form = document.querySelector('#addContactForm');
+console.log(form);
+function handleForm(event) {
+    event.preventDefault();
+    console.log(event.target);
+    const contact = {
+        id: new Date().toISOString(),
+        userName: event.target['contactName'].value,
+        email: event.target['contactEmail'].value,
+        phoneNumber: event.target['contactPhone'].value,
+        company: event.target['contactCompany'].value,
+        address: event.target['contactAddress'].value,
+    }
+    phoneBook.addContact(contact);
+    const contactList = document.querySelector('#contactList')
+    contactList.innerHTML = '';
+    phoneBook.contactsList.forEach((e, index) => {
+        if (index === 0) {
+            return;
+        }
+        const li = document.createElement('li');
+        li.textContent = `userName: ${e.userName}, email: ${e.email}, phoneNumber: ${e.phoneNumber}`;
+        contactList.append(li);
     })
-    if (index !== -1) {
-        this.contactsList[index] = updatedContact;
-    } else {
-        alert('Contact not found');
-    }
-},
-deleteContact(userName) {
-    const index = this.contactsList.findIndex(contact => {
-        contact.userName === userName;
-    });
-    if (index !== -1) {
-        this.contactsList.splice(index, 1);
-    } else {
-        alert('Contact not found');
-    }
 }
 
-console.log(new Date().getTime());//кол-во миллисекунд с 70 года 
+form.addEventListener('submit', handleForm);
+
